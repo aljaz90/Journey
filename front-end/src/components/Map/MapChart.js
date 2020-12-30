0521bbbb039a68c938b24b079462c75640d80abe
aljaz90 <aljaz1.kern@gmail.com>
2020-12-30 22:14:19 +0100

    Added basic vertical constraints to the map

--- front-end/src/components/Map/MapChart.js
+++ front-end/src/components/Map/MapChart.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Map, Marker, Overlay } from 'pigeon-maps';
   
-  
 export const MapChart = props => {
+
+    const [center, _setCenter] = useState([25, 0]);
+    const [zoom, setZoom] = useState(3);
+
+    const setCenter = (center) => {
+        if (center[0] > 78) {
+            center[0] = 78;
+        }
+        else if (center[0] < -78) {
+            center[0] = -78;
+        }
+
+        _setCenter(center);
+    };
 
     return (
         <div className="map">
             <Map 
-                defaultCenter={[25, 0]} 
-                defaultZoom={2.9}
+                minZoom={3}
+                center={center} 
+                zoom={zoom}
+                onBoundsChanged={({ center, zoom }) => { setCenter(center); setZoom(zoom); console.log(center) }}
                 attribution={false}
             >
                 <Marker anchor={[50.874, 4.6947]} payload={1} onClick={({ event, anchor, payload }) => {}} />
