6e3b6791ac7a2b4bc5b5280734fe95fa87c6157b
aljaz90 <aljaz1.kern@gmail.com>
2020-12-31 20:09:37 +0100

    Started working on new contraints

--- front-end/src/components/Map/MapChart.js
+++ front-end/src/components/Map/MapChart.js
@@ -1,29 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Map, Marker, Overlay } from 'pigeon-maps';
   
 export const MapChart = props => {
 
-    const [center, _setCenter] = useState([25, 0]);
-    const [zoom, setZoom] = useState(3);
+    const wrapper = useRef();
+    const [center, _setCenter] = useState([0, 0]/*[25, 0]*/);
+    const fullWidth = 2304;
+    const fullHeight = 1280;
 
-    const setCenter = (center) => {
+    const setCenter = (center, zoom) => {
+        if (!wrapper.current) {
+            return;
+        }
+
+        console.log((170 / zoom) * (wrapper.current.offsetHeight / fullHeight));
+
         if (center[0] > 78) {
             center[0] = 78;
+            _setCenter(center);
         }
         else if (center[0] < -78) {
             center[0] = -78;
+            _setCenter(center);
         }
 
-        _setCenter(center);
     };
 
     return (
-        <div className="map">
+        <div ref={wrapper} className="map">
             <Map 
-                minZoom={3}
+                minZoom={1}
+                defaultZoom={1.7}
                 center={center} 
-                zoom={zoom}
-                onBoundsChanged={({ center, zoom }) => { setCenter(center); setZoom(zoom); console.log(center) }}
+                onBoundsChanged={({ center, zoom }) => { setCenter(center, zoom); }}
                 attribution={false}
             >
                 <Marker anchor={[50.874, 4.6947]} payload={1} onClick={({ event, anchor, payload }) => {}} />
