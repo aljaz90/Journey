a6885f5e10cc02a147437fac4d6671adffd02334
aljaz90 <aljaz1.kern@gmail.com>
2021-01-02 10:19:51 +0100

    Changed map provider to Leaflet

--- front-end/src/components/Map/MapChart.js
+++ front-end/src/components/Map/MapChart.js
@@ -1,42 +1,35 @@
-import React, { useState, useRef } from 'react';
-import { Map, Marker, Overlay } from 'pigeon-maps';
+import React, { useState } from 'react';
+import Leaflet from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+let DefaultIcon = Leaflet.icon({
+            ...Leaflet.Icon.Default.prototype.options,
+            iconUrl: icon,
+            iconRetinaUrl: iconRetina,
+            shadowUrl: iconShadow
+        });
+        Leaflet.Marker.prototype.options.icon = DefaultIcon;
   
 export const MapChart = props => {
 
-    const wrapper = useRef();
-    const [center, _setCenter] = useState([0, 0]/*[25, 0]*/);
-    const fullWidth = 2304;
-    const fullHeight = 1280;
-
-    const setCenter = (center, zoom) => {
-        if (!wrapper.current) {
-            return;
-        }
-
-        console.log((170 / zoom) * (wrapper.current.offsetHeight / fullHeight));
-
-        if (center[0] > 78) {
-            center[0] = 78;
-            _setCenter(center);
-        }
-        else if (center[0] < -78) {
-            center[0] = -78;
-            _setCenter(center);
-        }
-
-    };
-
+    const position = [51.505, -0.09];
     return (
-        <div ref={wrapper} className="map">
-            <Map 
-                minZoom={1}
-                defaultZoom={1.7}
-                center={center} 
-                onBoundsChanged={({ center, zoom }) => { setCenter(center, zoom); }}
-                attribution={false}
-            >
-                <Marker anchor={[50.874, 4.6947]} payload={1} onClick={({ event, anchor, payload }) => {}} />
-            </Map>    
+        <div className="map">
+            <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+                <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+        </MapContainer>
         </div>
     );
 };
