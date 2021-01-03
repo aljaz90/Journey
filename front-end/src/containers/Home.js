451b7ebb88105e4b173e3a40e99dd9cb83375ec4
aljaz90 <aljaz1.kern@gmail.com>
2021-01-03 12:24:18 +0100

    Improved Auth loader

--- front-end/src/containers/Home.js
+++ front-end/src/containers/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 import { Dropdown } from '../components/Forms/Dropdown';
 import { MapChart } from '../components/Map/MapChart';
 import { getInitialsForName, isEmpty, signOut } from '../Utils';
@@ -18,26 +18,19 @@ export default class Home extends Component {
 
 
     render() {
+
+        if (isEmpty(this.props.user)) {
+            return <Redirect to="/signin" />;
+        }
+
         return (
         <div className="home">
             <div className="home--account--wrapper">
-                {
-                    !isEmpty(this.props.user) ?
-                        <Dropdown onSelect={opt => this.handleUserAction(opt)} staticButton={true} options={[{key: "edit", text: "Edit account"}, {key: "signout", text: "Sign out"}]} className="home--account--button" wrapperClassName="home--account--button--wrapper">
-                            <div style={{backgroundImage: `url(${this.props.user.imageUrl ? this.props.user.imageUrl.replaceAll(" ", "%20") : null})`}} className="home--account--icon">
-                                {this.props.user.imageUrl ? "" : getInitialsForName(this.props.user.name)}
-                            </div>
-                        </Dropdown>
-                    :
-                        <div className="home--account--signin">
-                            <NavLink activeClassName="nav--nav--item-active" className="nav--nav--item" to="/signin">
-                                Sign in
-                            </NavLink>
-                            <NavLink activeClassName="nav--nav--item-active" className="nav--nav--item" to="/signup">
-                                Sign up
-                            </NavLink>
-                        </div>
-                }
+                <Dropdown onSelect={opt => this.handleUserAction(opt)} staticButton={true} options={[{key: "edit", text: "Edit account"}, {key: "signout", text: "Sign out"}]} className="home--account--button" wrapperClassName="home--account--button--wrapper">
+                    <div style={{backgroundImage: `url(${this.props.user.imageUrl ? this.props.user.imageUrl.replaceAll(" ", "%20") : null})`}} className="home--account--icon">
+                        {this.props.user.imageUrl ? "" : getInitialsForName(this.props.user.name)}
+                    </div>
+                </Dropdown>
             </div>
             <MapChart />
         </div>
